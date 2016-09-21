a5a2cd4866c2792e445282cf7e93a214f19276f7
Emil Ahlbäck <e.ahlback@gmail.com>
2016-09-21 11:02:16 +0200

    add some comments to example

--- example.js
+++ example.js
@@ -15,8 +15,8 @@ const genPromise = (value) =>
 runSeq([
   runSeq([
     genPromise('1.1'),
-    () => genPromise('1.2'),
-    genPromise('1.3')
+    () => genPromise('1.2'), // this'll be called only when 1.1 finishes
+    genPromise('1.3') // this is called immediately
   ]),
   () => runSeq([
     () => genPromise('2.1'),
@@ -32,9 +32,9 @@ runSeq([
 1.1 finish
 1.2 start
 1.2 finish
-2.2 start
-2.1 start
-2.1 finish
-2.2 finish
+2.2 start // note how this started before 2.1
+2.1 start // and this started after
+2.1 finish // yet the return value is in the correct order
+2.2 finish // and this one comes after 2.2
 [ [ '1.1', '1.2', '1.3' ], [ '2.1', '2.2' ] ]
 */
