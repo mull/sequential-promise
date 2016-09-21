3b618d066d60c0e01288acb026642ecb247e5ecc
Emil Ahlbäck <e.ahlback@gmail.com>
2016-09-21 11:09:43 +0200

    add an example where a promise rejects

--- README.md
+++ README.md
@@ -1,8 +1,21 @@
 A simple way to run promises in a sequence.
 
+
+## How to use
+[See example.js which is a runnable example](/example.js)
+
+There's essentially two ways to run promises. The function expects an array of either type:
+- promise
+- function returning a promise
+
+Which one to use boils down to:
+- should the async operations start all at once
+- should the next operation only start when the previous one finishes?
+
+
 ## TODO
-- more examples
-- example of exception handling
+- better example of exception handling
+- tests for when promise rejects
 - option to stop on first failure
 
-[Example of usage](/example.js)
+
--- example.js
+++ example.js
@@ -12,29 +12,46 @@ const genPromise = (value) =>
   });
 
 
-runSeq([
-  runSeq([
-    genPromise('1.1'),
-    () => genPromise('1.2'), // this'll be called only when 1.1 finishes
-    genPromise('1.3') // this is called immediately
-  ]),
-  () => runSeq([
-    () => genPromise('2.1'),
-    genPromise('2.2'),
+function successfulExample() {
+  return runSeq([
+    runSeq([
+      genPromise('1.1'),
+      () => genPromise('1.2'), // this'll be called only when 1.1 finishes
+      genPromise('1.3') // this is called immediately
+    ]),
+    () => runSeq([
+      () => genPromise('2.1'),
+      genPromise('2.2'),
+    ])
+  ]).then(v => console.log("return value: ", v));
+
+  /* OUTPUT:
+  1.1 start
+  1.3 start
+  1.3 finish
+  1.1 finish
+  1.2 start
+  1.2 finish
+  2.2 start // note how this started before 2.1
+  2.1 start // and this started after
+  2.1 finish // yet the return value is in the correct order
+  2.2 finish // and this one comes after 2.2
+
+  return value: [ [ '1.1', '1.2', '1.3' ], [ '2.1', '2.2' ] ]
+  */
+}
+
+function rejectExample() {
+  return runSeq([
+    () => genPromise('first'),
+    () => new Promise((resolve, reject) => reject("no dice")),
+    () => genPromise('third'),
   ])
-]).then(v => console.log(v));
+    .then(v => console.log(v))
+    .catch(e => console.error("failed: ", e));
+}
 
-
-/* OUTPUT:
-1.1 start
-1.3 start
-1.3 finish
-1.1 finish
-1.2 start
-1.2 finish
-2.2 start // note how this started before 2.1
-2.1 start // and this started after
-2.1 finish // yet the return value is in the correct order
-2.2 finish // and this one comes after 2.2
-[ [ '1.1', '1.2', '1.3' ], [ '2.1', '2.2' ] ]
-*/
+console.log("Successful example: ")
+successfulExample()
+  .then(() => console.log("------------\nExample with rejecting promise:"))
+  .then(rejectExample);
