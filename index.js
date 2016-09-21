af0a27b82f80228bab5a49487a003d1f09cab26e
Emil Ahlbäck <e.ahlback@gmail.com>
2016-09-21 11:02:54 +0200

    indentation fix

--- index.js
+++ index.js
@@ -14,13 +14,13 @@ function runSeq(promises) {
         const promise = typeof entry === "function" ? entry() : entry;
 
         return promise
-                .then(save)
-                .then(iterate)
-                .then(() => setTimeout(loop, 0))
-                .catch(e => {
-                  reject(e);
-                  return Promise.reject(e);
-                });
+          .then(save)
+          .then(iterate)
+          .then(() => setTimeout(loop, 0))
+          .catch(e => {
+            reject(e);
+            return Promise.reject(e);
+          });
       } else {
         return resolve(values);
       }
